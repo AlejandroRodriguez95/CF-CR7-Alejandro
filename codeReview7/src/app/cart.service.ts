df4a89e537a96d6c4bc4b85618ed412403a688c0
Alejandro <alejandro.rodriguez95@gmail.com>
2020-10-24 00:27:52 +0200

    most of it is working

--- codeReview7/src/app/cart.service.ts
+++ codeReview7/src/app/cart.service.ts
@@ -9,8 +9,26 @@ export class CartService {
 
   addToCart(product){
     this.items.push(product);
-    console.table(this.items);
   }
+  
+  getTotalPrice(items){
+    let sum: number = 0;
+    for(let x in items)
+    {
+      sum  += this.items[x].price;
+    }
+
+    if (sum > 200 && sum < 500){
+      return sum*0.9;
+    }
+    else if(sum > 500){
+      return sum*0.8;
+    }
+    else{
+      return sum;
+    }
+  }
+
 
   getItems(){
     return this.items;
--- codeReview7/src/app/shopping-cart/shopping-cart.component.ts
+++ codeReview7/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.scss']
 })
+
 export class ShoppingCartComponent implements OnInit {
-  constructor() { }
+  items: any;
+  totalPrice: number;
+  constructor(private cartService: CartService) { }
 
 
   ngOnInit(): void {
+    this.items = this.cartService.getItems();
+    this.totalPrice = this.cartService.getTotalPrice(this.items);
+    console.log(this.totalPrice);
   }
 
 }
